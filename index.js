af64b2095fad68b2a6d587c74f0900ec394890a1
Matthew Ewan-Laws <matt@spacebetween.co.uk>
2019-07-26 11:17:53 +0100

    initial commit and starting rules

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ["airbnb-base"],
+    rules: {
+        "no-console": "off",
+        "no-prototype-builtins": "off",
+        "no-underscore-dangle": "off",
+    },
+}
